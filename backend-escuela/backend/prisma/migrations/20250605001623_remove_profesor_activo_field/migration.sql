cad67f7f8f6bc2dfe771be61bb65621fc7eafab0
2chris23 <124632588+2chris23@users.noreply.github.com>
2025-06-25 15:31:37 -0400

    Delete backend-escuela directory

--- backend-escuela/backend/prisma/migrations/20250605001623_remove_profesor_activo_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `activo` on the `Profesor` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Profesor" DROP COLUMN "activo";
--- backend-escuela/frontend/src/components/base/IconifyIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { Icon, IconProps } from '@iconify/react';
-
-const IconifyIcon = (props: IconProps) => {
-  return <Icon {...props} />;
-};
-
-export default IconifyIcon; 
